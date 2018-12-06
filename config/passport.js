ba3d95136de7061ca316e0c8cc10a53c58c368bd
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 17:59:51 +0200

    save new users to db

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load User Model
+const User = mongoose.model('users');
+
 module.exports = function(passport) {
   passport.use(
     new GoogleStrategy(
@@ -19,6 +22,27 @@ module.exports = function(passport) {
           0,
           profile.photos[0].value.indexOf('?')
         );
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image
+        };
+
+        // Check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // Return user
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
     )
   );
