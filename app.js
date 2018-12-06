13cb1dd278671c7c6bb30e6c8d1ccec4612f8dc8
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 17:07:31 +0200

    passport strategy config setup

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
