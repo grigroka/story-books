3e5468f39a7c6551c2dacd7648dfe0f790487d91
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 16:29:33 +0200

    home route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
