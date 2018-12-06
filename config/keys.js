ac5905da121843bc32bb6dc16d616468b7cf46fb
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-06 16:55:53 +0200

    api keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  mongoURI: 'mongodb://rokas:rokas1@ds127704.mlab.com:27704/storybooks-dev',
+  googleClientID:
+    '783445315485-cvmvcu4isaiqhvh99pe7kc2uv6miimbl.apps.googleusercontent.com',
+  googleClientSecret: 'a-c6sVWxUjGZuNodXBJClPuG'
+};
