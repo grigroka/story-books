66feba1aeb0c1d83860cede3ed9887aa7cea8356
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-07 11:25:52 +0200

    dashboard get user stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
 router.get('/', ensureGuest, (req, res) => {
@@ -9,7 +11,9 @@ router.get('/', ensureGuest, (req, res) => {
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('index/dashboard');
+  Story.find({ user: req.user.id }).then(stories => {
+    res.render('index/dashboard', { stories });
+  });
 });
 
 router.get('/about', (req, res) => {
