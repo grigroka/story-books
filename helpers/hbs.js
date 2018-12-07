1a3d373d293e247871582f3d6257bf2c2fcc5889
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-07 10:45:14 +0200

    truncate string helper function

---
+++ helpers/hbs.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  truncate: function(str, len) {
+    if (str.length > len && str.length > 0) {
+      let newStr = str + '';
+      newStr = str.substr(0, len);
+      newStr = str.substr(0, newStr.lastIndexOf(' '));
+      newStr = newStr.length > 0 ? newStr : str.substr(0, len);
+      return newStr + '...';
+    }
+    return str;
+  }
+};
