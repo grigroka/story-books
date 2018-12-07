c10546202f726bb8387ede74edfde09528c24618
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-07 12:41:14 +0200

    added comments functionality

--- routes/stories.js
+++ routes/stories.js
@@ -95,4 +95,21 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({ _id: req.params.id }).then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    };
+
+    // Add to comments array (unshift adds to array begining)
+    story.comments.unshift(newComment);
+
+    story.save().then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+  });
+});
+
 module.exports = router;
